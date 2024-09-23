f6c354930359e445e41fb8e69a57cc3a7a2fa8b4
PVS2008 <144795083+PVS2008@users.noreply.github.com>
2024-09-23 16:08:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação',
+    'O que é java script?',
+    'o Java srcipt é uma linguagem de programação'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
