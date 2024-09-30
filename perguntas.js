dbdfdda9dd4fd2238e1e106b2ea9d540f6096072
PVS2008 <144795083+PVS2008@users.noreply.github.com>
2024-09-30 15:51:57 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,18 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é java script?',
-    'o Java srcipt é uma linguagem de programação'
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
